aa3106fcff48671abdda59820653ea680cb7665c
Stefano <stefano.rb@gmail.com>
2015-04-29 13:41:19 -0700

    added top-level utility method files

---
+++ ProcessMemUsage.h
@@ -0,0 +1,54 @@
+/*
+ * ProcessMemUsage.h
+ *
+ *  Created on: Jan 7, 2013
+ *      Author: stef
+ */
+
+#ifndef PROCESSMEMUSAGE_H_
+#define PROCESSMEMUSAGE_H_
+
+#include <unistd.h>
+
+void process_mem_usage(double& vm_usage, double& resident_set)
+{
+   using std::ios_base;
+   using std::ifstream;
+   using std::string;
+
+   vm_usage     = 0.0;
+   resident_set = 0.0;
+
+   // 'file' stat seems to give the most reliable results
+   //
+   ifstream stat_stream("/proc/self/stat",ios_base::in);
+
+   // dummy vars for leading entries in stat that we don't care about
+   //
+   string pid, comm, state, ppid, pgrp, session, tty_nr;
+   string tpgid, flags, minflt, cminflt, majflt, cmajflt;
+   string utime, stime, cutime, cstime, priority, nice;
+   string O, itrealvalue, starttime;
+
+   // the two fields we want
+   //
+   unsigned long vsize;
+   long rss;
+
+   stat_stream >> pid >> comm >> state >> ppid >> pgrp >> session >> tty_nr
+               >> tpgid >> flags >> minflt >> cminflt >> majflt >> cmajflt
+               >> utime >> stime >> cutime >> cstime >> priority >> nice
+               >> O >> itrealvalue >> starttime >> vsize >> rss; // don't care about the rest
+
+   stat_stream.close();
+
+   long sz = sysconf(_SC_PAGESIZE);
+//   long page_size_kb = sysconf(_SC_PAGESIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+   long page_size_kb = sysconf(sz) / 1024; // in case x86-64 is configured to use 2MB pages
+   vm_usage     = vsize / 1024.0;
+   resident_set = rss * page_size_kb;
+}
+
+
+#endif /* PROCESSMEMUSAGE_H_ */
+
