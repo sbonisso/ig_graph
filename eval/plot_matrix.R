7f65580ab26d2d3d8fa536990101d711eda3ffc2
sbonisso <sbonisso@ucsd.edu>
2015-05-06 19:16:58 -0700

    added task/method to plot pairwise comparisons on unsupervised stanford_s22 predictions

--- eval/plot_matrix.R
+++ eval/plot_matrix.R
@@ -24,7 +24,68 @@ plot.heatmap <- function(d,output.plot) {
     plot(p);
     dev.off()
 }
-
+plot.full.heatmap <- function(d,output.plot) {
+    pdf(output.plot)
+    source("simple_theme.R")
+    d$X.name <- factor(d$X.name, levels(d$X.name)[order(d$X.name)])
+    d$variable <- factor(d$variable, levels(d$variable)[order(d$X.name)])
+    p <- (ggplot(d, aes(x=X.name,y=variable,fill=value))+
+          geom_tile()+
+          geom_text(aes(X.name, variable, label = value), color = "#073642", size = 4) +
+          ylab("") + xlab("") + 
+          ## scale_fill_gradientn(colours=brewer.pal(10,"RdYlBu"),
+          ##                       limits=c(0,1.0)) +
+          scale_fill_gradient(name="Jaccard index",
+                              low = "#fdf6e3", high = "steelblue",
+                              breaks=seq(0, 1, by = 0.2), limits = c(0.3, 1)) +
+          scale_x_discrete(expand = c(0, 0)) +
+          scale_y_discrete(expand = c(0, 0)) +          
+          ##simple_gdocs_theme() +
+          guides(fill = guide_colorbar(barwidth = 7, barheight = 1, title.position = "top",
+                     title.hjust = 0.5)) +
+          theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1), 
+                panel.grid.major = element_blank(),
+                panel.border = element_blank(),
+                panel.background = element_blank(),
+                axis.ticks = element_blank(),
+                legend.justification = c(1, 0),
+                ##legend.position = c(0.9, 1.05),
+                legend.position = "top",
+                legend.direction = "horizontal") +
+          guides(fill = guide_colorbar(barwidth = 7, barheight = 1, title.position = "top", 
+                     title.hjust = 0.5))
+          );
+    plot(p);
+    dev.off()
+}
+plot.half.heatmap <- function(d,output.plot) {
+    pdf(output.plot)
+    source("simple_theme.R")
+    p <- (ggplot(d, aes(x=X.name,y=variable,fill=value))+
+          geom_tile()+
+          geom_text(aes(X.name, variable, label = value), color = "#073642", size = 4) +
+          ylab("") + xlab("") + 
+          scale_fill_gradient(name="Jaccard index",
+                              low = "#fdf6e3", high = "steelblue",
+                              breaks=seq(0, 1, by = 0.2), limits = c(0.3, 1)) +
+          scale_x_discrete(expand = c(0, 0)) +
+          scale_y_discrete(expand = c(0, 0)) +          
+          guides(fill = guide_colorbar(barwidth = 7, barheight = 1, title.position = "top",
+                     title.hjust = 0.5)) +
+          theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1), 
+                panel.grid.major = element_blank(),
+                panel.border = element_blank(),
+                panel.background = element_blank(),
+                axis.ticks = element_blank(),
+                legend.justification = c(1, 0),
+                          legend.position = "top",
+                legend.direction = "horizontal") +
+          guides(fill = guide_colorbar(barwidth = 7, barheight = 1, title.position = "top", 
+                     title.hjust = 0.5))
+          );
+    plot(p);
+    dev.off()    
+}
 
 ###
 ### if run from command line
@@ -36,19 +97,50 @@ if(!interactive()) {
     output.plot <- args[2]
     plot.type <- args[3]
     if(length(args) != 3 && length(args) != 4) {
-        print("USAGE: input.csv output.pdf [heatmap] [max_rate]");
+        print("USAGE: input.csv output.pdf [heatmap/half_mat] [max_rate]");
         ##stopifnot(length(args) == 4);
     }
     max_rate <- if(length(args) == 4) as.numeric(args[4]) else 1.0
     ##
     ## read data
     ##
-    d <- read.csv(input.csv, sep=",", header=TRUE)  
+    
     ##
     ## plot the clone expression
     if(plot.type == "heatmap") {
+        d <- read.csv(input.csv, sep=",", header=TRUE)  
         d$total <- d$total / max_rate;
         plot.heatmap(d,output.plot);
+    } else if(plot.type == "half_mat") {
+        ## read in as matrix
+        d <- read.csv(input.csv, sep=",", header=TRUE, row.names=1)
+        d1 <- as.matrix(d)
+        ##print(d1);
+        ## remove lower triangle, add in diagonal
+        d1[lower.tri(d1)] <- NA
+        diag(d1) <- 1;
+        d2 <- data.frame(d1)
+        d2$X.name <- rownames(d2);
+        ##print(d2);
+        ## melt to long format
+        md <- melt(d2);
+        ##print(md)
+        ## remove NA 
+        md <- md[-which(is.na(md$value)),]
+        md2 <- data.frame(md)
+        
+        ## sort names by frequency
+        tn <- sort(table(md2$X.name), decreasing=TRUE)
+        
+        ## set as factor levels - based on freq
+        md2$X.name <- factor(md2$X.name, levels=factor(names(tn)))
+        md2$variable <- factor(md2$variable, levels=factor(names(tn)))
+        ##md2$X.name <- factor(md2$X.name, levels=c("VDJsolver","AbOrigin","IgBLAST","IMGT", "iHMMune", "JOINSOLVER"))
+        ##md2$variable <- factor(md2$variable, levels=c("VDJsolver","AbOrigin","IgBLAST","IMGT", "iHMMune", "JOINSOLVER"))
+        ##print(levels(md2$X.name))
+        ##print(levels(md2$variable))
+        
+        plot.half.heatmap(md2,output.plot);
     } else {
         print("INVALID plot type specified!");
     }
--- eval/rakefile.rb
+++ eval/rakefile.rb
@@ -1,5 +1,6 @@
 require 'rake/clean'  # for auto-cleaning
 require 'open3'
+require_relative 'run_data'
 
 namespace :iggraph do 
   num_proc = 8
@@ -10,7 +11,6 @@ namespace :iggraph do
   desc 'grid search over VJ/D k-mers for alleles'
   task :grid_search_k_allele do
     unless File.exists?("param_out_alleles.csv")
-      #data_f = "data/smab_data_mut10.fa"
       cmd = "./param_search.rb #{data_f} param_out_alleles.csv allele #{num_proc}"
       Open3.capture3(cmd)      
     end
@@ -23,9 +23,7 @@ namespace :iggraph do
       check_cmd = "#{smab_path} check -f #{data_f}"
       out_r,err,pip = Open3.capture3(check_cmd)
       max_rate = out_r.chomp.to_f
-      #puts "RATE = #{max_rate}"
       cmd = "./plot_matrix.R param_out_alleles.csv mat_allele.pdf heatmap #{max_rate}"
-      #puts cmd
       Open3.capture3(cmd)
     end
   end
@@ -34,7 +32,6 @@ namespace :iggraph do
   desc 'grid search over VJ/D k-mers for gene'
   task :grid_search_k_gene do
     unless File.exists?("param_out_gene.csv")
-      #data_f = "data/smab_data_mut10.fa"
       cmd = "./param_search.rb #{data_f} param_out_gene.csv gene #{num_proc}"
       Open3.capture3(cmd)
     end
@@ -52,5 +49,115 @@ namespace :iggraph do
     end
   end
   CLEAN << "mat_gene.pdf"
+
+  desc 'run on smAb data'
+  task :test_smab_data do 
+    unless File.exists?("smab_runs.csv")
+      tools = ["igblast", "iggraph", "ihmmune"]
+      File.open("smab_runs.csv", "w") do |f|
+        run_data_supervised("./data/smab_data_mut10.fa", f, 
+                            "./smab_run_pred", tools, "allele")
+        #run_data_supervised("../tests/data/ten_ab.fa", f, "./smab_run_pred", "allele")
+      end
+    end
+  end
+  CLEAN << "smab_runs.csv"
+  CLEAN << Dir.glob("smab_run_pred*")
+
+  desc 'compare smAb data predictions'
+  task :cmp_smab_data => :test_smab_data do 
+    out_file_base = "smab_data_comparison"
+    type = "allele"
+    unless !Dir.glob(out_file_base+"*").empty?
+      #
+      pred_lst = Dir.glob("./smab_run_pred*")
+      compare_unsupervised(pred_lst, out_file_base, "allele")
+      
+      # #
+      # f_lst = Dir.glob("./smab_run_pred*")
+      # tmp_f = Tempfile.new("filenames")
+      # tmp_f.puts f_lst
+      # tmp_f.close
+      # #
+      # name_f = Tempfile.new("names")
+      # f_lst.each do |s| 
+      #   name_f.puts s.split(".tab")[0].split("_")[-1].upcase        
+      # end
+      # name_f.close
+      # FileUtils.cp(name_f.path, "/tmp/test_names.txt")
+      # FileUtils.cp(tmp_f.path, "/tmp/test_file.txt")
+      # #
+      # valve_bin = "/home/stef/git_repos/valve/bin/valve"
+      # valve_cmd = 
+      #   "#{valve_bin} unsupervised -f #{tmp_f.path} -n #{name_f.path} -y #{type} -o #{out_file_base}"
+      # Open3.capture3(valve_cmd)
+    end
+  end
+  CLOBBER << Dir.glob("smab_data_comparison*")
+
+  desc 'run on stanford_s22 data'
+  task :test_stanford_s22_data do 
+    unless File.exists?("stanford_s22_runs.csv")
+      #tools = ["igblast", "iggraph", "ihmmune"]
+      tools = ["igblast", "iggraph"]
+      #tools = ["igblast"]
+      File.open("stanford_s22_runs.csv", "w") do |f|
+        run_data_unsupervised("./data/Stanford_S22_upcase.fasta",
+                              f, 
+                              "./stanford_s22_run_pred",
+                              tools, 
+                              "allele")
+      end
+    end
+  end
+  CLEAN << "stanford_s22_runs.csv"
+  CLEAN << Dir.glob("stanford_s22_run_pred*")
   
+  
+  desc 'compare Stanford_S22 data predictions'
+  task :cmp_stanford_s22_data => :test_stanford_s22_data do
+    out_file_base = "stanford_s22_data_comparison"
+    type = "allele"
+    unless !Dir.glob(out_file_base+"*").empty?
+      #
+      puts 'hi'
+      pred_lst = Dir.glob("./stanford_s22_run_pred*")
+      compare_unsupervised(pred_lst, out_file_base, "allele")
+    end
+  end
+  CLOBBER << Dir.glob("stanford_s22_data_comparison*")
+
+  desc 'compare all Stanford_S22 tools'
+  task :cmp_all_stanford_s22 do 
+    out_file_base = "stanford_s22_all_comparison"
+    name_re = Regexp.new(/(\w+)\_S22\_results\.txt$/)
+    unless !Dir.glob(out_file_base + "*").empty?
+      ["gene", "allele"].each do |y|
+        out_file_base_s = [out_file_base, y].join("_")
+        pred_lst = Dir.glob("./data/s22_results/*results.txt")        
+        #pred_lst = Dir.glob("./data/s22_results_few/*results.txt")
+        puts out_file_base_s
+        
+        name_lst = pred_lst.map{|s| name_re.match(s)[1]}
+        pred_lst.push("./data/stanford_s22_run_pred_iggraph.tab")
+        name_lst.push("IgGraph")
+        puts pred_lst.to_s
+        puts name_lst.to_s
+        compare_unsupervised(pred_lst, 
+                             out_file_base_s, 
+                             y, 
+                             name_lst)
+      end
+    end
+  end
+
+  desc 'plot all Stanford_S22'
+  task :plot_all_stanford_s22 => :cmp_all_stanford_s22 do 
+    unless File.exists?("stanford_s22_mat_V.pdf")
+      cmd = "./plot_matrix.R stanford_s22_all_comparison_allele_V.csv stanford_s22_mat_V.pdf half_mat"
+      Open3.capture3(cmd)
+    end
+  end
+  
+
 end
